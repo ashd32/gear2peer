cddb9d4270363d5e43abc3c447a1b0f0ab768a71
Sukjune Hong <sukjune.hong@gmail.com>
2018-11-14 15:54:02 -0500

    adds the puppet fucntion

---
+++ utils/scrape-rei.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
+
+module.exports = async (url) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const html = await page.content();
+    await page.screenshot({
+        path: 'rei.png',
+        fullPage: true
+    });
+    const $ = cheerio.load(html);
+    var title = $('h1.product-information-title').text().trim().replace(/\s{2,}/, " ");
+    console.log(title);
+    $('#rei').text(title);
+
+    const imageUrls = [];
+    $('div.no-select > img').each(function (i, image) {
+        imageUrls.push(`https://www.rei.com${$(image).attr('src').split('?')[0]}`);
+    });
+
+    await browser.close();
+
+    return {
+        title: title,
+        images: imageUrls
+    }
+
+};
